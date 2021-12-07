d39a6740b960e738c400fa9435ec94ecc5e0af00
raquel <raquelanaroman@gmail.com>
2021-12-07 14:40:00 -0500

    solved in ruby

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
@@ -1,15 +1,31 @@
 def find_first_duplicate(arr)
   # type your code in here
+  hash_map = Hash.new
+
+  arr.each do |el|
+    if hash_map.has_key?(el) 
+      return el
+    else
+      hash_map[el] = 1
+    end
+  end
+
+  return -1
 end
 
 if __FILE__ == $PROGRAM_NAME
-  puts "Expecting: 3"
-  puts "=>", find_first_duplicate([2, 1, 3, 3, 2])
+  puts "Expecting: 2"
+  puts "=>", find_first_duplicate([2, 2])
 
   puts
 
   puts "Expecting: -1"
   puts "=>", find_first_duplicate([1, 2, 3, 4])
+
+  puts
+  
+  puts "Expecting: 4"
+  puts "=>", find_first_duplicate([2, 4, 5, 4, 1, 2])
 
   # Don't forget to add your own!
 end
