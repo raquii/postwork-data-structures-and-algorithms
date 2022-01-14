38d23f098263f8282dfbc2ec0b431f75c6e627c1
raquel <raquelanaroman@gmail.com>
2022-01-13 21:24:30 -0500

    solve fibonacci seq

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
-  // type your code here
+  let fibSeq = [0, 1];
+
+  for (let i = 0; i < num; i++) {
+    let sum = fibSeq[0] + fibSeq[1];
+    fibSeq = [fibSeq[1], sum]
+  }
+
+  return fibSeq[0];
 }
 
 if (require.main === module) {
@@ -7,12 +14,11 @@ if (require.main === module) {
   console.log("Expecting: 0");
   console.log("=>", fibonacci(0));
 
-  console.log("");
+  console.log("Expecting: 8");
+  console.log("=>", fibonacci(6));
 
   console.log("Expecting: 1");
   console.log("=>", fibonacci(2));
-
-  console.log("");
 
   console.log("Expecting: 55");
   console.log("=>", fibonacci(10));
